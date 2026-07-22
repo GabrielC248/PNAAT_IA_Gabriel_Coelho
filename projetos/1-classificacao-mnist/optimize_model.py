14754f1f33709b4b009b652cbb419d3e6fcbaf2b
Gabriel Cavalcanti Coelho <gabrielccoelho10@gmail.com>
2026-07-22 18:47:00 -0300

    feat: carregamento do modelo

--- projetos/1-classificacao-mnist/optimize_model.py
+++ projetos/1-classificacao-mnist/optimize_model.py
@@ -13,3 +13,15 @@ import os
 # ---------------------------------------------------------------------------
 
 # insira seu código aqui
+
+model_path = "model.h5"
+tflite_path = "model.tflite"
+
+# -------- 1. Carregar o modelo treinado em "model.h5" --------
+# Verifica se o arquivo existe
+if not os.path.exists(model_path):
+    raise FileNotFoundError(
+        f'Modelo {model_path} não encontrado. Execute train_model.py primeiro.'
+    )
+
+model = tf.keras.models.load_model(model_path)
